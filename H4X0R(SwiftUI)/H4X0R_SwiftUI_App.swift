40d02317523cc4a8816c4505f60e82a819910674
nikita <kafes-nik@mail.ru>
2023-09-13 22:27:57 +0300

    Initial Commit

---
+++ H4X0R(SwiftUI)/H4X0R_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_SwiftUI_App.swift
+//  H4X0R(SwiftUI)
+//
+//  Created by Nikita Hasan Kafes on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
